db5ae139ad966ff85f67bd13c2a8120661997b4c
Jc Din <ozknightwalker62@gmail.com>
2017-05-08 17:36:12 +0800

    use class based views and separate url to ease on bug tracing and managements

--- app.py
+++ app.py
@@ -3,13 +3,9 @@ from __future__ import unicode_literals
 import os
 
 from sanic import Sanic
-from sanic.response import json
 
-app = Sanic()
+app = Sanic(__name__)
 
-
-@app.route("/")
-async def homepage(request):
-    return json({'hello': 'world'})
+from urls import *
 
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
