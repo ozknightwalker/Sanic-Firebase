7e77de8260a96a6383636212e548d9be18acea00
Jc Din <ozknightwalker62@gmail.com>
2017-05-08 17:40:51 +0800

    initial configuration for jinja2 usage

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ import os
 
 from sanic import Sanic
 
+from jinja2 import Environment, PackageLoader, select_autoescape
+
 app = Sanic(__name__)
+
+template_env = Environment(
+    loader=PackageLoader('jinja_example', 'templates'),
+    autoescape=select_autoescape(['html', 'xml']),
+    enable_async=True
+)
 
 from urls import *
 
